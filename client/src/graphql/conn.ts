b3be04e66bdba4df7212255ad34e08f5dc87f212
GrahamQuan <grahamquan24@gmail.com>
2023-10-30 22:57:42 +0800

    fix: uri

--- client/src/graphql/conn.ts
+++ client/src/graphql/conn.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const uri = 
   import.meta.env.MODE === 'production' 
-    ? `https://${import.meta.env.VITE_VERCEL_URL}` : import.meta.env.VITE_GRAPHQL_URI
+    ? `https://${import.meta.env.VITE_VERCEL_ENV}` : import.meta.env.VITE_GRAPHQL_URI
 
 const client = new ApolloClient({
   uri: `${uri}/api/graphql`,
